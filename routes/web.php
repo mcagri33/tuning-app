c73830f24406aead0a279d338142bfbf7fa96d95
cagri <mehmetcagrikaratas@gmail.com>
2022-12-05 10:38:20 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\PermissionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +23,36 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'role:admin'])->name('panel.')->prefix('panel')->group(function (){
+    Route::get('/',[IndexController::class,'index'])->name('index');
+});
+
+Route::group(['prefix' => 'panel/role', 'middleware' => 'auth', 'role:admin'], function () {
+    Route::get('/', [RoleController::class, 'index'])
+        ->name('admin.role.index');
+    Route::post('/store', [RoleController::class, 'store'])
+        ->name('admin.role.store');
+    Route::get('/edit/{id}', [RoleController::class, 'edit'])
+        ->name('admin.role.edit');
+    Route::post('/update', [RoleController::class, 'update'])
+        ->name('admin.role.update');
+    Route::get('/delete/{id}', [RoleController::class, 'destroy'])
+        ->name('admin.role.delete');
+});
+
+Route::group(['prefix' => 'panel/permissions', 'middleware' => 'auth', 'role:admin'], function () {
+    Route::get('/', [PermissionController::class, 'index'])
+        ->name('admin.permissions.index');
+    Route::post('/store', [PermissionController::class, 'store'])
+        ->name('admin.permissions.store');
+    Route::get('/edit/{id}', [PermissionController::class, 'edit'])
+        ->name('admin.permissions.edit');
+    Route::post('/update', [PermissionController::class, 'update'])
+        ->name('admin.permissions.update');
+    Route::get('/delete/{id}', [PermissionController::class, 'destroy'])
+        ->name('admin.permissions.delete');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

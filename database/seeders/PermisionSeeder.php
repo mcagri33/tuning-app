df74cd9569ac2502ffe6102c2301efc65d083919
cagri <mehmetcagrikaratas@gmail.com>
2022-12-06 12:05:57 +0300

    added permision seeder

---
+++ database/seeders/PermisionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermisionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'create roles']);
+        Permission::create(['name' => 'create permission']);
+    }
+}

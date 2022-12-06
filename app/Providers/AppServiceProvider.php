bc635c7c828b36e8fa57ca19b4c33e402477469e
cagri <mehmetcagrikaratas@gmail.com>
2022-12-06 14:53:49 +0300

    added language global function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Language;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrap();
+
+        $language_data = Language::get();
+        if(!session()->get('session_short_name')) {
+            $current_short_name = Language::where('is_default','Yes')->first()->short_name;
+        } else {
+            $current_short_name = session()->get('session_short_name');
+        }
+        $default_lang_data = Language::where('is_default','Yes')->first();
+
+        view()->share('global_cms_language_data',$language_data);
+        view()->share('global_cms_short_name',$default_lang_data->short_name);
     }
 }

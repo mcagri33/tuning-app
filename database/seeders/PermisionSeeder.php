69d4a3ceea2bf96d3bb66e3df516817c2dddc95f
cagri <mehmetcagrikaratas@gmail.com>
2022-12-07 13:04:52 +0300

    added permission seeder

--- database/seeders/PermisionSeeder.php
+++ database/seeders/PermisionSeeder.php
@@ -18,5 +18,11 @@ class PermisionSeeder extends Seeder
         Permission::create(['name' => 'create user']);
         Permission::create(['name' => 'create roles']);
         Permission::create(['name' => 'create permission']);
+        Permission::create(['name' => 'create language']);
+        Permission::create(['name' => 'settings']);
+        Permission::create(['name' => 'create car brands']);
+        Permission::create(['name' => 'create car models']);
+        Permission::create(['name' => 'create car brains']);
+
     }
 }
